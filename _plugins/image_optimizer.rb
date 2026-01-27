197d99aed0107bc6e5c0513e8323defb179363c2
Jonas Ekuan <70990222+jekuan1@users.noreply.github.com>
2026-01-27 18:28:18 +0000

    improving quality of images

--- _plugins/image_optimizer.rb
+++ _plugins/image_optimizer.rb
@@ -7,14 +7,14 @@ Jekyll::Hooks.register :site, :post_write do |site|
   Dir.glob(File.join(image_dir, '**', '*.{jpg,jpeg,png}')).each do |img_path|
     image = MiniMagick::Image.open(img_path)
     image.strip
-    image.quality "85"
+    image.quality "95"
     image.resize "1200>"
     image.write img_path
     
     # Create WebP version
     webp_path = img_path.sub(/\.(jpg|jpeg|png)$/i, '.webp')
     image.format 'webp'
-    image.quality "85"
+    image.quality "95"
     image.write webp_path
   end
 end
